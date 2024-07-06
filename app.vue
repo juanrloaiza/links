189c46acd6ad6999dfbc7e890f02410a4dadfdde
Juan R. Loaiza <juanrloaiza@gmail.com>
2024-07-06 12:58:50 -0400

    Fetch links from yaml

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+const { data } = await useAsyncData('linkslist', () => queryContent('/links').findOne())
+
+const links = data.value.body
+
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <a v-for="link in links" :href="link.url">{{ link.name }}</a>
   </div>
 </template>
